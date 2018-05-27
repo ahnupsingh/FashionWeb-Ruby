f0bdbdcb3e26dfe211c0698277d7dd8041dfd82d
ahnupsingh <anup.singh2071@gmail.com>
2018-05-27 11:13:13 +0545

    Deploy to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def welcome
     render html: "Welcome to New Fashion Tailors"
   end
+
+  def goodbye
+    render html: "Thank you for the visit. See you next time"
+  end
 end
