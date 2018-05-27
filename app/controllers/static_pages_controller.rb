5979ff0f24bac97ffb06c477454ccf43302fd784
ahnupsingh <anup.singh2071@gmail.com>
2018-05-28 00:46:18 +0545

    Basic view of static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def services
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | New Fashion Tailors"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | About Us"
+  end
+
+  test "should get services" do
+    get static_pages_services_url
+    assert_response :success
+    assert_select "title", "Services | Services we provide"
   end
 
 end
