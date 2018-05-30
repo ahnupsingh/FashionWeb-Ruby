513b7863aa62935f6d9f890a693517a06b3b3787
ahnupsingh <anup.singh2071@gmail.com>
2018-05-30 21:10:13 +0545

    validate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   has_many:complete
-  validates:name, presence: true
-  validates:email, presence: true
+  validates:name, presence: true, length: {maximum: 30}
+  validates:email, presence: true,
+                   length: {maximum: 128},
+                   format: {with: VALID_EMAIL_REGEX}
+                  #  ,uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_063654) do
+ActiveRecord::Schema.define(version: 2018_05_30_095846) do
 
   create_table "completes", force: :cascade do |t|
     t.integer "billNo"
